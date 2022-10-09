cfce77ba6a55be4173f27619960dd2714ffa8541
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-10-09 17:21:25 +0300

    Add PropTypes

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,17 +1,26 @@
-import { FriendList } from "./FriendsList.styled"
-import FriendListItem from "./FriendsListItem"
+import PropTypes from 'prop-types';
+import { FriendList } from './FriendsList.styled';
+import FriendListItem from './FriendsListItem';
 
 export default function FriendsList({ friends }) {
   return (
     <FriendList>
-      {friends.map(friend => 
+      {friends.map(friend => (
         <FriendListItem
           key={friend.id}
           isOnline={friend.isOnline}
           avatar={friend.avatar}
           name={friend.name}
         />
-      )}
+      ))}
     </FriendList>
-  )
-}
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
--- src/components/FriendsList/FriendsListItem.jsx
+++ src/components/FriendsList/FriendsListItem.jsx
@@ -1,12 +1,28 @@
-import {ListItem, StatusOnline, StatusOffline, Avatar, Name} from './FriendsListItem.styled'
+import PropTypes from 'prop-types';
+import {
+  ListItem,
+  StatusOnline,
+  StatusOffline,
+  Avatar,
+  Name,
+} from './FriendsListItem.styled';
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <ListItem>
-      {isOnline ? <StatusOnline></StatusOnline> : <StatusOffline></StatusOffline>}
+      {isOnline ? (
+        <StatusOnline></StatusOnline>
+      ) : (
+        <StatusOffline></StatusOffline>
+      )}
       <Avatar src={avatar} alt={name} />
       <Name>{name}</Name>
     </ListItem>
-    
   );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 };
